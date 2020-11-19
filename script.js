f6d6b8d5beb338050be4d98fe45b30c422e739a7
laratarpley13 <ltarp13@gmail.com>
2020-11-19 09:59:40 -0600

    added pseudocode to script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+//variables to store api keys
+
+//variables to store base urls
+
+//displayResults function
+
+//getBooks function
+    //use fetch to call NYT Books API
+        //call displayResults function on responseJson
+
+//getVideos function
+    //use fetch to call YouTube API
+        //call displayResults function on responseJson
+
+//watchForm function
+function watchForm() {
+    //listen for submission
+    //prevent default form behavior
+    //validate form entries
+    //call the getBooks function
+}
